7ec2a669c72349bc977bbdac0c4da1117014ba7b
FruitStudioUK <contact.fruitstudio@gmail.com>
2025-08-12 15:46:06 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Hero } from '@/components/Hero';
-import { Services } from '@/components/Services';
-import { Portfolio } from '@/components/Portfolio';
+import { Services } from '@/components/Services'; 
+{/* import { Portfolio } from '@/components/Portfolio'; */}
 import { Contact } from '@/components/Contact';
 
 export default function Home() {
