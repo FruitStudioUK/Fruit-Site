d5b01b1ee2a1ae6869a4b57caf55b17a6ac3ad6b
FruitStudioUK <contact.fruitstudio@gmail.com>
2025-08-12 15:32:05 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
         'text-white': '#fff8e6',
         'text-gray': '#d6c3b4',
         'text-dark': '#140c04',
-        'text-lightdark': '#392714',
+        'text-lightdark': '#392815',
       },
       fontFamily: {
         nunito: ['var(--font-nunito)', 'sans-serif'], // changed from inter → nunito
