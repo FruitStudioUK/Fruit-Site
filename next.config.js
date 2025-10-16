8dda334f4066dc7f09f603700a35467d0bbf7b68
FruitStudioUK <contact.fruitstudio@gmail.com>
2025-10-16 18:05:13 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,31 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+
+  async redirects() {
+    return [
+      {
+        source: '/dashboard',
+        destination: '/overview',
+        permanent: true,
+      },
+      {
+        source: '/campaigns',
+        destination: '/benefits',
+        permanent: true,
+      },
+      {
+        source: '/partners',
+        destination: '/about',
+        permanent: true,
+      },
+      {
+        source: '/support',
+        destination: '/partner-support',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
