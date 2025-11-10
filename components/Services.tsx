28bbb52d0f80423a7b217839d0a26374545991b2
FruitStudioUK <contact.fruitstudio@gmail.com>
2025-11-10 00:40:01 +0000

    Update Services.tsx

--- components/Services.tsx
+++ components/Services.tsx
@@ -132,7 +132,7 @@ export function Services() {
                 <ul className="space-y-2">
                   {service.features.map((feature, idx) => (
                     <li key={idx} className="flex items-center space-x-2">
-                      <div className="w-2 h-2 text-gray rounded-full"></div>
+                      <div className="w-2 h-2 text-white rounded-full"></div>
                       <span className="font-nunito font-bold text-dark">
                         {feature}
                       </span>
@@ -175,7 +175,7 @@ export function Services() {
                 <ul className="space-y-2">
                   {service.features.map((feature, idx) => (
                     <li key={idx} className="flex items-center space-x-2">
-                      <div className="w-2 h-2 text-gray rounded-full"></div>
+                      <div className="w-2 h-2 text-white rounded-full"></div>
                       <span className="font-nunito font-bold text-dark">
                         {feature}
                       </span>
